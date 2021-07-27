c940a6d37876313676fe4b1d80f6e5ec86a9a898
akshaysinha6599 <akshaysinha6599@gmail.com>
2021-07-27 04:19:48 +0000

    Used parent child relationship

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import Comp1 from './components/Comp1'
 function App() {
   return (
     <div className="App">
-     
+     <Comp1 tezab="Hey1"/>
+     <Comp1 tezab="Hey2"/>
+     <Comp1 tezab="Hey3"/>
     </div>
   );
 }
---
+++ src/components/Comp1.js
@@ -0,0 +1,11 @@
+//import './App.css';
+
+function Comp1({tezab}) {
+  return (
+    <div className="App">
+        {tezab} Akki
+    </div>
+  );
+}
+
+export default Comp1;
